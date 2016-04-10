56f0539028f0e807ce787410c97655baf7feb873
Konstantin Martini <k@martini.pm>
2016-04-10 23:27:03 +0200

    cleaned up setup.py and the likes

--- setup.py
+++ setup.py
@@ -1,32 +1,44 @@
 #!/usr/bin/env python
 
-from setuptools import setup
-
+from setuptools import setup, find_packages
 from versioning import get_cmdclass, get_version
 
 def read_requirements(name='requirements.txt'):
+    """ Read reqiurements.txt file into a list of lines, that are
+        non-empty, and do not start with a `#`-character.
+
+        >>> len(read_requirements(__file__)) > 23
+        True
+    """
 
     with open(name, 'r') as fh:
-        requirements = [l.strip() for l in fh.readlines() if l.strip()]
+        lines = [l.strip() for l in fh.readlines() if l.strip()]
 
-    return requirements
+    return [r for r in lines if not r.startswith('#')]
 
 setup_args = dict(
     name='asciidrumming',
-    version=get_version(),
-    cmdclass=get_cmdclass(),
     description='Ascii-based (drum-)sequencer.',
     url='http://github.com/isnok/asciidrumming',
     author='Konstantin Martini',
     author_email='k@tuxcode.org',
     license='MIT',
-    packages=['asciidrumming'],
+)
+
+setup_args.update(
+    version=get_version(),
+    cmdclass=get_cmdclass(),
+    packages=find_packages(),
     install_requires=read_requirements(),
-    entry_points = {
-        'console_scripts': ['ascii_drummer=asciidrumming.cli:cli'],
-    },
     include_package_data=True,
     zip_safe=False,
 )
 
-if __name__ == '__main__': setup(**setup_args)
+setup_args.update(
+    entry_points={
+        'console_scripts': ['ascii_drummer=asciidrumming.cli:cli'],
+    },
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
